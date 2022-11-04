1eabcb97f9bbc9edcf423fb640f231dd923f2763
Orathai25 <113133031+Orathai25@users.noreply.github.com>
2022-11-04 11:48:51 +0700

    .commit

--- Home.py
+++ Home.py
@@ -6,8 +6,6 @@ import plotly.express as px
 import plotly.graph_objects as go
 import pickle
 
-st.balloons()
-
 st.image('./image/banner.png')
 
 html_8="""
--- Home2.py
+++ Home2.py
@@ -7,7 +7,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 from plotly.subplots import make_subplots
-
+st.balloons()
 
 #st.image("./pic/01.png")
 
